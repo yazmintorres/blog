c618924bb7bb78831a0c9ab63be95991e2d41966
Yazmin <yazminmoniquetorres@gmail.com>
2023-04-04 13:22:51 -0700

    Create instances of Express Router

---
+++ server/controllers/post.js
@@ -0,0 +1,5 @@
+const addPost = (req, res) => {
+  res.json("from controller");
+};
+
+module.exports = addPost;
--- server/server.js
+++ server/server.js
@@ -3,11 +3,19 @@ const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
 const db = require("./db/db-connection.js");
+const postRoutes = require("./routes/posts.js");
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/users.js");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
+
+// manage routes and controllers using this file
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // creates an endpoint for the route "/""
 app.get("/", (req, res) => {
